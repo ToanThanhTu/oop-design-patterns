eee549a0dd4840b9442c5ce4f8757e86655c50d4
trevor <toantu3005@gmail.com>
2026-03-21 17:25:36 +1100

    feat: add iterator class

---
+++ apps/01-taskflow/backend/src/models/task/taskIterator.ts
@@ -0,0 +1,66 @@
+import type { Task } from "#models/task/task.js"
+import type { TaskTypeType } from "#models/task/types.js"
+
+interface FilterType {
+  assignee?: string
+  dueDateFrom?: string
+  dueDateTo?: string
+  label?: string
+  priority?: string
+  type?: TaskTypeType
+}
+
+export class TaskIterator implements Iterable<Task> {
+  private filters: FilterType
+  private tasks: Task[]
+
+  constructor(tasks: Task[]) {
+    this.tasks = tasks
+    this.filters = {}
+  }
+
+  filterBy(filters: FilterType): this {
+    this.filters = filters
+    return this
+  }
+
+  *[Symbol.iterator](): Generator<Task> {
+    for (const task of this.tasks) {
+      if (this.matchesFilters(task)) {
+        yield task
+      }
+    }
+  }
+
+  private matchesFilters(task: Task): boolean {
+    if (this.filters.assignee && task.getAssignee() !== this.filters.assignee) {
+      return false
+    }
+
+    if (this.filters.priority && task.getPriority() !== this.filters.priority) {
+      return false
+    }
+
+    if (this.filters.type && task.getType() !== this.filters.type) {
+      return false
+    }
+
+    const taskDueDate = task.getDueDate()
+
+    if (this.filters.dueDateFrom || this.filters.dueDateTo) {
+      if (!taskDueDate) {
+        return false
+      }
+
+      if (this.filters.dueDateFrom && taskDueDate < this.filters.dueDateFrom) {
+        return false
+      }
+
+      if (this.filters.dueDateTo && taskDueDate > this.filters.dueDateTo) {
+        return false
+      }
+    }
+
+    return true
+  }
+}
